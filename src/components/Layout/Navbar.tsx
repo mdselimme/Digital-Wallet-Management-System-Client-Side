16c992142aeeae9b2be20da857ee621d061a6dd1
mdselimme <mdselimdev@gmail.com>
2025-08-22 04:34:52 +0600

    add: header footer logo and font

--- src/components/Layout/Navbar.tsx
+++ src/components/Layout/Navbar.tsx
@@ -1,11 +1,117 @@
+import {
+  NavigationMenu,
+  NavigationMenuItem,
+  NavigationMenuLink,
+  NavigationMenuList,
+} from "@/components/ui/navigation-menu";
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@/components/ui/popover";
+import { Button } from "../ui/button";
+import DigiPayLogo from "@/assets/images/Logo";
+import { Link, NavLink } from "react-router";
 
+// Navigation links array to be used in both desktop and mobile menus
+const navigationLinks = [
+  { path: "/", route: "Home" },
+  { path: "/about", route: "About" },
+  { path: "/feature", route: "Feature" },
+  { path: "/pricing", route: "Pricing" },
+  { path: "/faq", route: "Faq" },
+  { path: "/contact", route: "Contact" },
+];
 
-const Navbar = () => {
-    return (
-        <div>
-            <h1>navbar</h1>
+export default function Component() {
+  return (
+    <header className="border-b py-3 px-4 md:px-6">
+      <div className="container mx-auto">
+        <div className="flex h-16 justify-between gap-4">
+          {/* Left side */}
+          <div className="flex gap-2">
+            <div className="flex items-center md:hidden">
+              {/* Mobile menu trigger */}
+              <Popover>
+                <PopoverTrigger asChild>
+                  <Button className="group size-8" variant="ghost" size="icon">
+                    <svg
+                      className="pointer-events-none"
+                      width={16}
+                      height={16}
+                      viewBox="0 0 24 24"
+                      fill="none"
+                      stroke="currentColor"
+                      strokeWidth="2"
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      xmlns="http://www.w3.org/2000/svg"
+                    >
+                      <path
+                        d="M4 12L20 12"
+                        className="origin-center -translate-y-[7px] transition-all duration-300 ease-[cubic-bezier(.5,.85,.25,1.1)] group-aria-expanded:translate-x-0 group-aria-expanded:translate-y-0 group-aria-expanded:rotate-[315deg]"
+                      />
+                      <path
+                        d="M4 12H20"
+                        className="origin-center transition-all duration-300 ease-[cubic-bezier(.5,.85,.25,1.8)] group-aria-expanded:rotate-45"
+                      />
+                      <path
+                        d="M4 12H20"
+                        className="origin-center translate-y-[7px] transition-all duration-300 ease-[cubic-bezier(.5,.85,.25,1.1)] group-aria-expanded:translate-y-0 group-aria-expanded:rotate-[135deg]"
+                      />
+                    </svg>
+                  </Button>
+                </PopoverTrigger>
+                <PopoverContent align="start" className="w-36 p-1 md:hidden">
+                  <NavigationMenu className="max-w-none *:w-full">
+                    <NavigationMenuList className="flex-col items-start gap-0 md:gap-2">
+                      {navigationLinks.map((link, index) => (
+                        <NavigationMenuItem key={index} className="w-full">
+                          <NavigationMenuLink asChild className="py-1.5">
+                            <NavLink to={link.path}>{link.route}</NavLink>
+                          </NavigationMenuLink>
+                        </NavigationMenuItem>
+                      ))}
+                    </NavigationMenuList>
+                  </NavigationMenu>
+                </PopoverContent>
+              </Popover>
+            </div>
+            {/* Main nav */}
+            <div className="flex items-center gap-6">
+              <Link to={"/"} className="text-primary hover:text-primary/90">
+                <DigiPayLogo width={120} height={50} />
+              </Link>
+              {/* Navigation menu */}
+              <NavigationMenu className="h-full *:h-full max-md:hidden">
+                <NavigationMenuList className="h-full gap-2">
+                  {navigationLinks.map((link, index) => (
+                    <NavigationMenuItem key={index} className="h-full">
+                      <NavigationMenuLink
+                        asChild
+                        className="text-muted-foreground hover:text-primary border-b-primary hover:border-b-primary data-[active]:border-b-primary h-full justify-center rounded-none border-y-2 border-transparent py-1.5 font-medium hover:bg-transparent data-[active]:bg-transparent!"
+                      >
+                        <NavLink to={link.path}>{link.route}</NavLink>
+                      </NavigationMenuLink>
+                    </NavigationMenuItem>
+                  ))}
+                </NavigationMenuList>
+              </NavigationMenu>
+            </div>
+          </div>
+          {/* Right side */}
+          <div className="flex items-center gap-2">
+            <Button
+              asChild
+              variant="ghost"
+              size="lg"
+              className="text-sm text-[#ffffff] bg-[#C53678]"
+            >
+              <Link to={"/login"}>Sign In</Link>
+            </Button>
+          </div>
         </div>
-    );
-};
-
-export default Navbar;
+      </div>
+    </header>
+  );
+}
--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,5 +1,7 @@
 import App from "@/App";
 import AboutPage from "@/pages/AboutPage";
+import Login from "@/pages/Authentication/Login";
+import Register from "@/pages/Authentication/Register";
 import ContactPage from "@/pages/ContactPage";
 import FaqPage from "@/pages/FaqPage";
 import FeaturePage from "@/pages/FeaturePage";
@@ -7,41 +9,47 @@ import HomePage from "@/pages/HomePage";
 import PricingPage from "@/pages/PricingPage";
 import { createBrowserRouter } from "react-router";
 
-
-
 export const router = createBrowserRouter([
-    {
-        path: "/",
-        Component: App,
-        children:[
-            {
-                index:true,
-                Component:HomePage,
-            },
-            {
-                path: "home",
-                Component:HomePage,
-            },
-            {
-                path: "about",
-                Component:AboutPage,
-            },
-            {
-                path: "feature",
-                Component:FeaturePage,
-            },
-            {
-                path: "pricing",
-                Component:PricingPage,
-            },
-            {
-                path: "contact",
-                Component:ContactPage,
-            },
-            {
-                path: "faq",
-                Component:FaqPage,
-            },
-        ]
-    }
-])
+  {
+    path: "/",
+    Component: App,
+    children: [
+      {
+        index: true,
+        Component: HomePage,
+      },
+      {
+        path: "home",
+        Component: HomePage,
+      },
+      {
+        path: "about",
+        Component: AboutPage,
+      },
+      {
+        path: "feature",
+        Component: FeaturePage,
+      },
+      {
+        path: "pricing",
+        Component: PricingPage,
+      },
+      {
+        path: "contact",
+        Component: ContactPage,
+      },
+      {
+        path: "faq",
+        Component: FaqPage,
+      },
+    ],
+  },
+  {
+    path: "/login",
+    Component: Login,
+  },
+  {
+    path: "/register",
+    Component: Register,
+  },
+]);
